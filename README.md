8344bd5733f14381091d651b7045e03df6695c98
jreitz2 <jreitz2@gmail.com>
2022-12-31 10:49:11 -0500

    basic functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # library
+
+This library app allows the user to compile a list of books in order to keep track of certain information about them, including whether they have been read or not. The user will be able to add and remove books from the list. 
---
+++ app.js
@@ -0,0 +1,109 @@
+//read button
+const isRead = document.querySelector(".is-read");
+isRead.addEventListener("click", () => {
+    isRead.classList.toggle("read");
+})
+
+//add card
+const library = document.querySelector(".library");
+// const addBtn = document.querySelector(".add");
+// addBtn.addEventListener("click", () => {
+//     const newCard = document.createElement('div');
+//     newCard.classList.add("card");
+//     library.appendChild(newCard);
+// })
+
+//pop-up form
+const newForm = document.querySelector(".new-form");
+const addBtn = document.querySelector(".add");
+addBtn.addEventListener("click", () => {
+    newForm.style.zIndex = "2";
+    newForm.style.border = "5px solid rgb(0, 0, 129, .5)";
+    newForm.style.boxShadow = "10px 5px 5px rgb(0, 0, 129, .5)";
+    newForm.style.display = "grid";
+})
+
+//close pop-up
+const closeBtn = document.querySelector(".close");
+closeBtn.addEventListener("click", () => {
+    newForm.style.display = "none";
+})
+
+//library
+let myLibrary = [];
+
+//submit button
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+const theForm = document.querySelector("#the-form");
+theForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const book3 = new Book(theForm.title.value, theForm.author.value, theForm.pages.value);
+    myLibrary.push(book3);
+    removeAllChildNodes(library);
+    addBookToLibrary();
+    newForm.title.value = "";
+    newForm.author.value = "";
+    newForm.pages.value = "";
+    newForm.style.display = "none";
+})
+
+//book constructor
+function Book(title, author, pages,) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+}
+
+// const book1 = new Book("title1", "author1", 111);
+// const book2 = new Book("crazy", "King", 234);
+// myLibrary.push(book1, book2);
+// console.table(myLibrary);
+
+//add books to display
+function addBookToLibrary () {
+        let libraryLength = myLibrary.length;
+            myLibrary.forEach(book => {
+            const newCard = document.createElement('div');
+            newCard.classList.add("card");
+            const newTitle = document.createElement('h4');
+            newTitle.textContent = `${book.title}`;
+            const newAuthor = document.createElement('h6');
+            newAuthor.textContent = `by ${book.author}`;
+            const newPages = document.createElement('p');
+            newPages.textContent = `${book.pages}`;
+            const newButton = document.createElement('button');
+            newButton.textContent = `Read`;
+            const closeButton = document.createElement('div');
+            closeButton.classList.add("close");
+            closeButton.style.position = "relative";
+            closeButton.textContent = `X`;
+            //remove book from library/close card
+            closeButton.addEventListener("click", () => {
+                myLibrary.splice(myLibrary.indexOf(book),1);
+                removeAllChildNodes(library);
+                addBookToLibrary();
+            })
+            newButton.classList.add("is-read");
+            newButton.addEventListener("click", () => {
+                newButton.classList.toggle("read");
+            })
+            library.appendChild(newCard);
+            newCard.appendChild(closeButton);
+            newCard.appendChild(newTitle);
+            newCard.appendChild(newAuthor);
+            newCard.appendChild(newPages);
+            newCard.appendChild(newButton);
+
+            newCard.setAttribute("id", myLibrary.indexOf(book));
+           
+})}
+addBookToLibrary();
+
+
+
+
